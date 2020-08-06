4ff72cf2205cacba44d72ac232c7cf1502404700
Narek Okroyan <narek.okroyan@outlook.com>
2020-08-06 15:36:03 +0400

    added single player mode

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const cells = Array.from(document.querySelectorAll('.cell'));
 const par = document.querySelector('#p');
-const start = document.querySelector('.start');
+const first = document.querySelector('.buttons>button');
+const startdiv = document.querySelector('.startdiv');
+const start = document.querySelector('.after>button');
 const input1 = document.querySelector('[name="player1"]');
 const input2 = document.querySelector('[name="player2"]');
-const container = document.querySelector('#container');
+const container = document.querySelector('.after');
 const body = document.querySelector('body');
+const select = document.querySelector('select');
+
 const Gameboard = (() => {
     let board = ['','','','','','','','',''];
     const getBoard = () => board;
@@ -35,14 +39,44 @@ const Gameboard = (() => {
 const player = (name, marker) => {
     return {name, marker};
 }
+const game = (() => {
+    let players = [];
+    let currentPlayer = 0;
+    let AI = false;
+    const play = () => {
+        cells.forEach(cell => cell.addEventListener('click', cellClicked));
+        Gameboard.clear();
+        Gameboard.display();
+        par.textContent = '';
+    }
+    function makeMove() {
+        const board = Gameboard.getBoard();
+        let pos = Math.floor(Math.random() * 9);
+        while(board[pos] !== '') pos = Math.floor(Math.random() * 9);
+        Gameboard.update(pos, game.players[game.currentPlayer].marker);
+        Gameboard.display();
+        if(Gameboard.checkForWin(game.players[game.currentPlayer].marker))  handleWin();
+        else if(Gameboard.checkForDraw()) handleDraw();
+        game.currentPlayer = (game.currentPlayer === 0) ? 1 : 0;
+        cells[pos].removeEventListener('click', cellClicked);
+    }
+    return {play, currentPlayer, players, AI, makeMove};
+})();
 function cellClicked() {
     const pos = parseInt(this.dataset.position);
     Gameboard.update(pos, game.players[game.currentPlayer].marker);
     Gameboard.display();
-    if(Gameboard.checkForWin(game.players[game.currentPlayer].marker))  handleWin();
-    else if(Gameboard.checkForDraw()) handleDraw();
+    if(Gameboard.checkForWin(game.players[game.currentPlayer].marker)) {  
+        handleWin();
+        return;
+    }
+    else if(Gameboard.checkForDraw()) {
+        handleDraw();
+        return
+    }
     game.currentPlayer = (game.currentPlayer === 0) ? 1 : 0;
     this.removeEventListener('click', cellClicked);
+    if(game.AI) game.makeMove();
 }
 function handleWin() {
     cells.forEach(cell => cell.removeEventListener('click', cellClicked));
@@ -57,24 +91,35 @@ const setUp = (player1, player2) => {
     game.players = [];
     game.players.push(player1, player2);
 };
-function startGame() {
-    const player1 = player(input1.value, 'X');
-    const player2 = player(input2.value, 'O');
-    game.setUp(player1, player2);
-    this.textContent = 'Restart';
+function startMultiPlayer() {
     game.currentPlayer = 0;
     game.play();
     body.style.backgroundColor = '#181a1b';
 }
-const game = (() => {
-    let players = [];
-    let currentPlayer = 0;
-    const play = () => {
-        cells.forEach(cell => cell.addEventListener('click', cellClicked));
-        Gameboard.clear();
-        Gameboard.display();
-        par.textContent = '';
+function startSinglePlayer() {
+    game.currentPlayer = 0;
+    body.style.backgroundColor = '#181a1b';
+    game.AI = true;
+    game.play();
+}
+function startFirstTime() {
+    startdiv.style.display = 'none';
+    container.style.display = 'flex';
+    const player1 = player(input1.value, 'X');
+    let player2;
+    if (select.value === "1") {
+        player2 = player("AI", 'O');
+        setUp(player1, player2);
+        startSinglePlayer();
+        start.addEventListener('click', startSinglePlayer);
     }
-    return {setUp, play, currentPlayer, players};
-})()
-start.addEventListener('click', startGame);
+    else {
+        player2 = player(input2.value, 'O');
+        setUp(player1, player2);
+        startMultiPlayer();
+        start.addEventListener('click', startMultiPlayer);
+    }
+}
+
+first.addEventListener('click', startFirstTime);
+
